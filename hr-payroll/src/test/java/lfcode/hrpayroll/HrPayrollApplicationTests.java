b98afda52ae29eb091d23d89c2debc6cc76b92c1
lfpo2005 <lfpo2005@yahoo.com.br>
2021-11-11 17:43:48 -0300

    Project hr-payroll created

---
+++ hr-payroll/src/test/java/lfcode/hrpayroll/HrPayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package lfcode.hrpayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrPayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
