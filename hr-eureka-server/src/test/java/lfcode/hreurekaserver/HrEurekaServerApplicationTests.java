f4eabfd7776ff6dbe13d0761e329a5088d921c98
lfpo2005 <lfpo2005@yahoo.com.br>
2021-11-11 23:37:39 -0300

    Project hr-eureka-server created

---
+++ hr-eureka-server/src/test/java/lfcode/hreurekaserver/HrEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package lfcode.hreurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
